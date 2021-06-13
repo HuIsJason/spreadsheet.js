40df1d7ca9fbc995939cbdcd6cc4d605b3ee0a9d
Jason Hu <jasonn.hu@mail.utoronto.ca>
2021-06-13 00:41:28 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
-/* server.js - Express server*/
 'use strict';
-const log = console.log;
-log('Express server');
 
 const express = require('express');
-const app = express();
-
 const path = require('path');
+
+const app = express();
 
 app.use(express.static(path.join(__dirname, '/pub')));
 
@@ -16,5 +13,5 @@ app.get('/examples.html', (req, res) => {
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  log(`Listening on port ${port}...`);
+  console.log(`Listening on port ${port}...`);
 });
