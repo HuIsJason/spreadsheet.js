3e5959cfe21537d1363de2722548ce112a1d6573
Jason Hu <jasonn.hu@mail.utoronto.ca>
2021-06-13 00:58:40 -0400

    Update examples.js

--- pub/examples.js
+++ pub/examples.js
@@ -68,9 +68,3 @@ const e3 = Spreadsheet('#expenses').createSpreadsheet(
       alert(`Your table is currently ${tableArray.length} rows long!`),
   }
 );
-
-[
-  ['TD', '10000', '1000000', '34535', 'Toronto', '2021-04-06'],
-  ['RBC', '50000', '325345', '76868', 'Markham', '2020-06-22'],
-  ['AmEx', '10000000', '5644', '76054', 'New York City', '2020-07-06'],
-];
